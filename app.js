54a3d185438e7b62f1de908030cbedc7dbc3dd48
Matheus Catalan Ferreira <matheuscatalan@Outlook.com>
2021-03-12 11:01:38 -0300

    asa

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
+import translations from
+
 $(document).ready(function () {
+  localStorage.language = "brazilian"
+
   $(window).scroll(function () {
     if (this.scrollY > 20) {
       $(".navbar").addClass("sticky")
@@ -6,4 +10,41 @@ $(document).ready(function () {
       $(".navbar").removeClass("sticky")
     }
   })
+
+  $(".menu-btn").click(function (e) {
+    $(".navbar .menu").toggleClass("active")
+    $(".menu-btn i").toggleClass("active")
+  })
 })
+
+function toggleFAB(fab) {
+  if (document.querySelector(fab).classList.contains("show")) {
+    document.querySelector(fab).classList.remove("show")
+  } else {
+    document.querySelector(fab).classList.add("show")
+  }
+}
+
+document.querySelector(".fab .main").addEventListener("click", function () {
+  toggleFAB(".fab")
+})
+
+function changeLanguage() {
+  var language = $("#btn-language").attr("language")
+
+  console.log(translations)
+
+  $(".main").removeClass(localStorage.language)
+  $(".main").addClass(language)
+  $("#btn-language").removeClass(language)
+  $("#btn-language").addClass(localStorage.language)
+
+  $("#btn-language").attr("language", localStorage.language)
+  $("#label-language").text(
+    localStorage.language == "brazilian" ? "Português" : "English"
+  )
+
+  localStorage.setItem("language", language)
+
+  toggleFAB(".fab")
+}
